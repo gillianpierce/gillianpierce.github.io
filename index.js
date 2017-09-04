135280704ee6e407c8dc9d4da03b96ab00e02e26
Gillian Pierce <gmpierce@ualberta.ca>
2017-09-03 23:18:29 -0600

    initial one page site for further development

--- index.js
+++ index.js
@@ -1,7 +1,49 @@
+
 $(document).ready(function(){
 
-  $(".main-page").hide().fadeIn(800).click(function(){
-    console.log("clicked")
-  });
+
+
+ $(".download").click(function(e){
+ 	e.preventDefault();  
+    window.location.href = 'resume.pdf';
+ }).mouseenter(function(){
+ 	$(this).addClass("mouse-over")
+ }).mouseleave(function(){
+ 	$(this).removeClass("mouse-over")
+ })
+
+$("#e-mail").mouseenter(function(){
+ 	$(this).addClass("mouse-over")
+ }).mouseleave(function(){
+ 	$(this).removeClass("mouse-over")
+ }).click(function(e){
+ 	window.location.href = 'mailto:gmpierce@ualberta.ca';
+ })
+
+ $("#github").mouseenter(function(){
+ 	$(this).addClass("mouse-over")
+ }).mouseleave(function(){
+ 	$(this).removeClass("mouse-over")
+ }).click(function(e){
+ 	window.location.href = "https://github.com/gillianpierce";
+ })
+
+  $("#linkedIn").mouseenter(function(){
+ 	$(this).addClass("mouse-over")
+ }).mouseleave(function(){
+ 	$(this).removeClass("mouse-over")
+ }).click(function(e){
+ 	window.location.href = "https://ca.linkedin.com/in/gillian-pierce";
+ })
+
+   $("#instagram").mouseenter(function(){
+ 	$(this).addClass("mouse-over")
+ }).mouseleave(function(){
+ 	$(this).removeClass("mouse-over")
+ }).click(function(e){
+ 	window.location.href = "https://www.instagram.com/_armagillo/" ;
+ })
+
+
 
 })
