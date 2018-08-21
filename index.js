d11a49767f54689846a341fe88ec2f91fd99a867
Gillian Pierce <gmpierce@ualberta.ca>
2018-08-20 18:40:14 -0600

    updated layout

--- index.js
+++ index.js
@@ -1,49 +1,73 @@
 
 $(document).ready(function(){
+<<<<<<< HEAD
 
+	var lastScrollTop = $(window).scrollTop();
+	var list = $('#list').children();
+	for(var i = 0, len = list.length; i < len; i++){
+		console.log($("#"+list[i].id).data("parent"))
+		if($("#"+$("#"+list[i].id).data("parent")).isInViewport()){
+			var current = $("#"+list[i].id);
+			current.addClass("active").removeClass("inactive");
+			break;
+		}
+	}
+	console.log(list);
+	$(window).scroll(function(){
+		console.log($(window).scrollTop())
+		if($(window).scrollTop()==0){
+			current.removeClass("active").addClass("inactive");
+			$("#"+$("#list").children()[0].id).removeClass("inactive").addClass("active");
+		}
+		else{
+		current = $('.active');
+		//console.log(current);
+		var parent = $('#'+current.data("parent"));
+		//console.log(parent);
+		var st = $(this).scrollTop();
+		if (! parent.isInViewport()){
+			if (st > lastScrollTop){
+				var next = $(current.next());
+				next.removeClass("inactive").addClass("active");
+				current.removeClass("active").addClass("inactive");
+				current = next;
+   			} else {
+   				var prev = $(current.prev());
+				prev.removeClass("inactive").addClass("active");
+				current.removeClass("active").addClass("inactive");
+				current = prev;
+   			}
+		}
 
+   		lastScrollTop = st;
+   	}
+	});
+=======
+ 
+>>>>>>> 12abf9b25dc986b7b171912029e8737cbf436dbf
 
- $(".download").click(function(e){
- 	e.preventDefault();  
-    window.location.href = 'resume.pdf';
- }).mouseenter(function(){
- 	$(this).addClass("mouse-over")
- }).mouseleave(function(){
- 	$(this).removeClass("mouse-over")
- })
+	// $(".nav-element.inactive").hover(function(){
+	// 	$(this).removeClass("inactive").addClass("active");
+	// }, function(){
+	// 	$(this).removeClass("active").addClass("inactive");
+	// })
 
-$("#e-mail").mouseenter(function(){
- 	$(this).addClass("mouse-over")
- }).mouseleave(function(){
- 	$(this).removeClass("mouse-over")
- }).click(function(e){
- 	window.location.href = 'mailto:gmpierce@ualberta.ca';
- })
-
- $("#github").mouseenter(function(){
- 	$(this).addClass("mouse-over")
- }).mouseleave(function(){
- 	$(this).removeClass("mouse-over")
- }).click(function(e){
- 	window.location.href = "https://github.com/gillianpierce";
- })
-
-  $("#linkedIn").mouseenter(function(){
- 	$(this).addClass("mouse-over")
- }).mouseleave(function(){
- 	$(this).removeClass("mouse-over")
- }).click(function(e){
- 	window.location.href = "https://ca.linkedin.com/in/gillian-pierce";
- })
-
-   $("#instagram").mouseenter(function(){
- 	$(this).addClass("mouse-over")
- }).mouseleave(function(){
- 	$(this).removeClass("mouse-over")
- }).click(function(e){
- 	window.location.href = "https://www.instagram.com/_armagillo/" ;
- })
-
-
+	$(".name-header").hide().fadeIn(2000);
+	$(".degree-header").hide().fadeIn(2000);
+	//
+	// $(".nav-element").click(function(){
+	// 	var target = $(this)[0].dataset.parent;
+	// 	$([document.documentElement, document.body]).animate({
+  //       scrollTop: $("#"+target).offset().top
+  //   }, 2000);
+	// })
 
 })
+
+$.fn.isInViewport = function() {
+	var elementTop = $(this).offset().top;
+	var elementBottom = elementTop + $(this).outerHeight();
+	var viewportTop = $(window).scrollTop();
+	var viewportBottom = viewportTop + $(window).height();
+	return elementBottom > viewportTop && elementTop < viewportBottom;
+};
